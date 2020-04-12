3f45ed23af7d6f15b1fbc7307128787a999d67f9
Dave Borca <50729531+dndvorks@users.noreply.github.com>
2020-04-12 15:10:14 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ function animateWithRandomNumber(myClass, from, to) {
 const itemsDown = [".light4", ".light5", ".light6", ".light7", ".light8", ".light11", ".light12", ".light13", ".light14", ".light15", ".light16"]
 .forEach(e => animateWithRandomNumber(e, -1080, 1080))
 const itemsUp = [".light1", ".light2", ".light3",".light9", ".light10", ".light17"]
-.forEach(e => animateWithRandomNumber(e, 1080, -1080))
+.forEach(e => animateWithRandomNumber(e, 1080, -1080));
 
 const controller = new ScrollMagic.Controller();
 
