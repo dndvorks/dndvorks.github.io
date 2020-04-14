1371f2f209933054b31bd577a0e484335d111cae
Dave Borca <50729531+dndvorks@users.noreply.github.com>
2020-04-14 17:14:44 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -27,71 +27,71 @@ const itemsUp = [".light1", ".light2", ".light3",".light9", ".light10", ".light1
 //Smooth Scrolling Transition
 
 
-var html = document.documentElement;
-var body = document.body;
+// var html = document.documentElement;
+// var body = document.body;
 
-var scroller = {
-  target: document.querySelector("#scroll-container"),
-  ease: 0.05, // <= scroll speed
-  endY: 0,
-  y: 0,
-  resizeRequest: 1,
-  scrollRequest: 0,
-};
+// var scroller = {
+//   target: document.querySelector("#scroll-container"),
+//   ease: 0.05, // <= scroll speed
+//   endY: 0,
+//   y: 0,
+//   resizeRequest: 1,
+//   scrollRequest: 0,
+// };
 
-var requestId = null;
+// var requestId = null;
 
-TweenLite.set(scroller.target, {
-  rotation: 0.01,
-  force3D: true
-});
+// TweenLite.set(scroller.target, {
+//   rotation: 0.01,
+//   force3D: true
+// });
 
-window.addEventListener("load", onLoad);
+// window.addEventListener("load", onLoad);
 
-function onLoad() {    
-  updateScroller();  
-  window.focus();
-  window.addEventListener("resize", onResize);
-  document.addEventListener("scroll", onScroll); 
-}
+// function onLoad() {    
+//   updateScroller();  
+//   window.focus();
+//   window.addEventListener("resize", onResize);
+//   document.addEventListener("scroll", onScroll); 
+// }
 
-function updateScroller() {
+// function updateScroller() {
   
-  var resized = scroller.resizeRequest > 0;
+//   var resized = scroller.resizeRequest > 0;
     
-  if (resized) {    
-    var height = scroller.target.clientHeight;
-    body.style.height = height + "px";
-    scroller.resizeRequest = 0;
-  }
+//   if (resized) {    
+//     var height = scroller.target.clientHeight;
+//     body.style.height = height + "px";
+//     scroller.resizeRequest = 0;
+//   }
       
-  var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+//   var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
 
-  scroller.endY = scrollY;
-  scroller.y += (scrollY - scroller.y) * scroller.ease;
+//   scroller.endY = scrollY;
+//   scroller.y += (scrollY - scroller.y) * scroller.ease;
 
-  if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
-    scroller.y = scrollY;
-    scroller.scrollRequest = 0;
-  }
+//   if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+//     scroller.y = scrollY;
+//     scroller.scrollRequest = 0;
+//   }
   
-  TweenLite.set(scroller.target, { 
-    y: -scroller.y 
-  });
+//   TweenLite.set(scroller.target, { 
+//     y: -scroller.y 
+//   });
   
-  requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
-}
+//   requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+// }
 
-function onScroll() {
-  scroller.scrollRequest++;
-  if (!requestId) {
-    requestId = requestAnimationFrame(updateScroller);
-  }
-}
+// function onScroll() {
+//   scroller.scrollRequest++;
+//   if (!requestId) {
+//     requestId = requestAnimationFrame(updateScroller);
+//   }
+// }
 
-function onResize() {
-  scroller.resizeRequest++;
-  if (!requestId) {
-    requestId = requestAnimationFrame(updateScroller);
-  }
-}
+// function onResize() {
+//   scroller.resizeRequest++;
+//   if (!requestId) {
+//     requestId = requestAnimationFrame(updateScroller);
+//   }
+// }
