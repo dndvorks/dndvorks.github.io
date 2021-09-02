8c0dc6acc8ee79a9bc9fc97d9a5b41380d69ed52
Dave Borca <50729531+dndvorks@users.noreply.github.com>
2021-09-03 00:29:07 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-
+$(window).ready(function(){
+window.location.href = 'https://dndvorks-7d0b90.ingress-baronn.easywp.com/';
+});
 var textWrapper = document.querySelector('.txt-1');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='txt1'>$&</span>");
 var textWrapper = document.querySelector('.txt-2');
